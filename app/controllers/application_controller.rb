a3a76b0778bb852659f6ece8cdc9f38ae9ee3633
Azraf Chowdhury <achowdhur@ltu.edu>
2019-02-17 17:42:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
     @article = Article.create(:title => params[:title], :content => params[:content], :id => params[:id])
     @article.save
 
-    redirect  "/articles/#{@article.id}"
+    redirect  "/articles/#{Article.id}"
   end
 
 end
